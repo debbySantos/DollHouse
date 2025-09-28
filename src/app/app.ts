25bdfda30cde7cb21037b525f30afcb397493470
Vanessa Balbino <vanessafmbalbino@gmail.com>
2025-09-27 22:49:06 -0300

    Ajustes visuais e funcionais no carrinho, imagem do produto e botões

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Carrinho } from './carrinho/carrinho';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Carrinho],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
