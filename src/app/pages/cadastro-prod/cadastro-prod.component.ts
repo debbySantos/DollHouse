86085ede87f786cdd61e4d6e0e39c9405ffc8282
Kayk Matheus <kayk.mslim@gmail.com>
2025-11-11 22:16:12 -0300

    Validação das bonecas

--- src/app/pages/cadastro-prod/cadastro-prod.component.ts
+++ src/app/pages/cadastro-prod/cadastro-prod.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core'; 
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { MenuLateralAdmComponent } from '../../shared/menu-lateral-adm/menu-lateral-adm.component';
 import { Bonecas } from '../../core/types/types';
 import { CadastroProdutoService } from '../../core/services/cadastro-produto.service';
-import { FormsModule} from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms'; 
 import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-cadastro-prod',
@@ -13,18 +14,17 @@ import { CommonModule } from '@angular/common';
   templateUrl: './cadastro-prod.component.html',
   styleUrl: './cadastro-prod.component.css'
 })
-
-
 export class CadastroProdComponent {
-  bonecaId?: number;
+  
+  @ViewChild('formBoneca') form!: NgForm;
 
+  bonecaId?: number;
   boneca: Bonecas = {} as Bonecas;
 
   constructor(
     private service: CadastroProdutoService,
     private router: Router,
     private route: ActivatedRoute
-
   ) {
     this.bonecaId = Number(this.route.snapshot.params['id'])
     if (this.bonecaId) {
@@ -35,7 +35,8 @@ export class CadastroProdComponent {
           this.boneca.categoria = boneca.categoria;
           this.boneca.estoque = boneca.estoque;
           this.boneca.preco = boneca.preco;
-          this.boneca.quantidade = boneca.quantidade;
+          // Assumindo que 'quantidade' e 'status' também vêm do objeto
+          this.boneca.quantidade = boneca.quantidade; 
           this.boneca.status = boneca.status;
           this.boneca.foto = boneca.foto;
           this.boneca.descricao = boneca.descricao
@@ -48,18 +49,22 @@ export class CadastroProdComponent {
     const file: File = event.target.files[0];
     if (file) {
       const reader = new FileReader();
-
       reader.onload = (e) => {
-
         this.boneca.foto = e.target?.result as string;
       };
-
-
       reader.readAsDataURL(file);
     }
   }
 
   submeter() {
+    // Se o formulário (do HTML) for inválido, não faz nada
+    if (this.form.invalid) {
+      console.log('Formulário inválido!');
+      this.form.control.markAllAsTouched(); // Mostra todos os erros
+      return;
+    }
+
+    // Se for válido, continua com sua lógica original:
     if (this.bonecaId) {
       this.service.alterarBoneca(this.boneca).subscribe(() => {
         this.router.navigate(['/gerenciamentoEstoque'])
