ce5dfdf192e313db70e2e662d2dc553dc2c69e2a
debbySantos <deborassantos880@gmail.com>
2025-11-18 21:51:45 -0300

    adicionando o footer e corrigindo os cards de depoimentos

---
+++ src/app/pages/todos-os-produtos/todos-os-produtos.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TodosOsProdutosComponent } from './todos-os-produtos.component';
+
+describe('TodosOsProdutosComponent', () => {
+  let component: TodosOsProdutosComponent;
+  let fixture: ComponentFixture<TodosOsProdutosComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TodosOsProdutosComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(TodosOsProdutosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/todos-os-produtos/todos-os-produtos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todos-os-produtos',
+  standalone: true,
+  imports: [],
+  templateUrl: './todos-os-produtos.component.html',
+  styleUrl: './todos-os-produtos.component.css'
+})
+export class TodosOsProdutosComponent {
+
+}
